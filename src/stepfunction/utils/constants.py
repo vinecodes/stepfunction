5d45d180f83e63a1ba46c1ba309d18699bd7a46a
Vineeth Penugonda <vineeth.penugonda@blumetra.com>
2024-10-02 19:48:20 +0530

    Add constants for logging and environment variables

---
+++ src/stepfunction/utils/constants.py
@@ -0,0 +1,12 @@
+""" Constants for the project. """
+
+# Logging
+DEFAULT_LOG_LEVEL = "INFO"
+"""str: The default logging level for the project."""
+
+DEFAULT_LOGGING_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+"""str: The default logging format for the project."""
+
+# Environment variables
+ENVIRONMENT_VARIABLE_LOG_LEVEL = "LOG_LEVEL"
+""" str: The environment variable for the logging level."""
